fa6cec52dc8d7c78ef39f78da80b9caf2fc957e7
Bishal Ghimire <bishal.ghimire@gmail.com>
2013-06-18 14:13:08 +0545

    Initial Commit

---
+++ BGMessageSMS/BGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BGAppDelegate.h
+//  BGMessageSMS
+//
+//  Created by Bishal Ghimire on 6/18/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BGMessageSMS/BGViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGViewController.h
+//  BGMessageSMS
+//
+//  Created by Bishal Ghimire on 6/18/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGViewController : UIViewController
+
+@end
