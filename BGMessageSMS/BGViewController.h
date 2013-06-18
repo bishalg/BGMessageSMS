0b2a2f69a73a30d1f93a931ff7c07d4e4c1e8a83
Bishal Ghimire <bishal.ghimire@gmail.com>
2013-06-18 14:54:51 +0545

    First Commit

--- BGMessageSMS/BGViewController.h
+++ BGMessageSMS/BGViewController.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
 
 @interface BGViewController : UIViewController
+<MFMessageComposeViewControllerDelegate> {
+    
+}
+/*/Volumes/Time Machine Backups/Work Projects/BGMessageSMS/BGMessageSMS/BGViewController.m:26:37: Assigning to 'id<MFMessageComposeViewControllerDelegate>' from incompatible type 'BGViewController *const __strong'*/
+
+@property (weak, nonatomic) IBOutlet UITextField *txtPhoneNo;
+@property (weak, nonatomic) IBOutlet UITextField *txtBody;
 
 @end
--- BGMessageSMS/BGViewController.m
+++ BGMessageSMS/BGViewController.m
@@ -14,16 +14,60 @@
 
 @implementation BGViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+#pragma mark -
+#pragma mark Outlets
+
+- (IBAction)btnSend:(id)sender {
+    MFMessageComposeViewController *controller = [[MFMessageComposeViewController alloc] init];
+	if([MFMessageComposeViewController canSendText])
+	{
+		controller.body = self.txtBody.text;
+		controller.recipients = [NSArray arrayWithObjects:self.txtPhoneNo.text, nil];
+		controller.messageComposeDelegate = self;
+        [self presentViewController:controller animated:YES completion:^{
+            NSLog(@"presntViewController Completed");
+        }];
+	}
+    [self dismissKeyBoard];
 }
 
-- (void)didReceiveMemoryWarning
-{
+-(void) dismissKeyBoard {
+    [self.txtPhoneNo resignFirstResponder];
+    [self.txtBody resignFirstResponder];
+}
+
+#pragma mark -
+#pragma mark MessageComposeDelegate
+
+-(void)messageComposeViewController:(MFMessageComposeViewController *)controller
+                didFinishWithResult:(MessageComposeResult)result {
+    switch (result) {
+		case MessageComposeResultCancelled:
+			NSLog(@"Cancelled");
+			break;
+		case MessageComposeResultFailed:
+            NSLog(@"MessageComposeResultFailed");
+			break;
+		case MessageComposeResultSent:
+            NSLog(@"MessageComposeResultSent");
+			break;
+		default:
+			break;
+	}
+    [self dismissViewControllerAnimated:YES completion:^{
+        NSLog(@"message Controller Dismissed");
+    }];
+    [self dismissKeyBoard];
+}
+
+#pragma mark View
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
